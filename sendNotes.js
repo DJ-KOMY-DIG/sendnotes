ad51753acef82b0ccb3b6ff2528bd14a12965cee
DJ <152664227+DJ-KOMY-DIG@users.noreply.github.com>
2026-01-27 20:18:40 +0900

    change

--- sendNotes.js
+++ sendNotes.js
@@ -31,7 +31,7 @@ if (sendBtn) {
     //    const noteVal = noteField.value.trim();
        const noteVal = noteField.value;
 
-        console.log("送信データ確認:", JSON.stringify(noteVal));
+        alert("送信データの中身:\n" + JSON.stringify(noteVal));
 
         // 入力がない場合
         if (!noteVal) return;
