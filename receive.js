8e6f577bae6e4d3745287bc4ff9efcf67ba98a82
DJ <152664227+DJ-KOMY-DIG@users.noreply.github.com>
2026-01-29 18:04:26 +0900

    change icon shadow

--- receive.js
+++ receive.js
@@ -29,13 +29,11 @@ const db = getDatabase(app);
 
 //   <rect x="5" y="8" width="11" height="13" rx="2" />
 
+// クリップボードのアイコン
 const ICON_COPY = `
-<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  
-  <rect x="6" y="7" width="11" height="13" rx="2" />
-  
-  <rect x="10" y="3" width="11" height="13" rx="2" fill="white" />
-  
+<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">  
+  <rect x="6" y="7" width="11" height="13" rx="2" fill="white" />  
+  <rect x="10" y="3" width="11" height="13" rx="2" fill="white" />  
 </svg>`;
 
 // チェックマークのアイコン（完了用）
