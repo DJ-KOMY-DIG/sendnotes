d581135d0bcbb1828b5d379a4d500fc76df684cc
DJ <152664227+DJ-KOMY-DIG@users.noreply.github.com>
2026-01-29 18:01:16 +0900

    Change Icon shadow

--- receive.js
+++ receive.js
@@ -27,10 +27,12 @@ const db = getDatabase(app);
 //   <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/>
 // </svg>`;
 
+//   <rect x="5" y="8" width="11" height="13" rx="2" />
+
 const ICON_COPY = `
 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   
-  <rect x="5" y="8" width="11" height="13" rx="2" />
+  <rect x="6" y="7" width="11" height="13" rx="2" />
   
   <rect x="10" y="3" width="11" height="13" rx="2" fill="white" />
   
